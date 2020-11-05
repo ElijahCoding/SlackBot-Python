9f4906a386e40bafd915bdb8161d3f4f9aefa029
ElijahCoding <cuiyeqing960904@gmail.com>
2020-11-05 23:24:17 +0300

    webhook setting up

--- bot.py
+++ bot.py
@@ -13,6 +13,18 @@ load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
 
+slack_event_adapter = SlackEventAdapter(
+    os.environ['SIGNING_SECRET'],
+    '/slack/events',
+    app
+)
+
 client = slack_sdk.WebClient(token=os.environ['SLACK_TOKEN'])
 
-client.chat_postMessage(channel='#test', text='hello')
+# client.chat_postMessage(channel='#test', text='hello')
+
+@slack_event_adapter.on('message')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
